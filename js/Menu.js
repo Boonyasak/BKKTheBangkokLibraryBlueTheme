beec1987c000d3144a5753ab128aeab0ef023916
Boonyasak Kaewpriwan <kaewpriwan@gmail.com>
2020-07-07 12:43:47 +0700

    First Commint for the branch theme

---
+++ js/Menu.js
@@ -0,0 +1,19 @@
+
+transparent = true;
+hasTransparent = false;
+
+$(document).scroll(function() {
+   if(hasTransparent){
+        if( $(this).scrollTop() > 260 ) {
+            if(transparent) {
+                transparent = false;
+                $('nav[role="navigation"]').removeClass('navbar-transparent');
+            }
+        } else {
+            if( !transparent ) {
+                transparent = true;
+                $('nav[role="navigation"]').addClass('navbar-transparent');
+            }
+        }
+    }
+});
